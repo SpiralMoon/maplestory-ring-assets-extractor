b1ceb6210e2597fbc9ebb3fbb301012c1fb7d254
SpiralMoon <dev.spiralmoon@gmail.com>
2025-09-16 19:17:56 +0900

    feat: Add wz initialize

--- RingAssetsExtractor/Program.cs
+++ RingAssetsExtractor/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Extensions.Configuration;
 using RingAssetsExtractor;
+using WzComparerR2.WzLib;
 
+Console.WriteLine("Start");
 
 #region load configuration
 
@@ -26,3 +28,40 @@ Directory.CreateDirectory(imagesDir);
 Console.WriteLine("Initialize output directories...");
 
 #endregion
+
+string wzFilePath = settings.Paths.WzFilePath; // Base.wz file path
+Wz_Structure wz = new Wz_Structure(); // Root WZ tree
+
+#region open wz (source from WzComparerR2)
+
+Console.WriteLine("Trying open Base.wz");
+
+if (string.Equals(Path.GetExtension(wzFilePath), ".ms", StringComparison.OrdinalIgnoreCase))
+{
+    wz.LoadMsFile(wzFilePath);
+}
+else if (wz.IsKMST1125WzFormat(wzFilePath))
+{
+    wz.LoadKMST1125DataWz(wzFilePath);
+    if (string.Equals(Path.GetFileName(wzFilePath), "Base.wz", StringComparison.OrdinalIgnoreCase))
+    {
+        string packsDir = Path.Combine(Path.GetDirectoryName(Path.GetDirectoryName(wzFilePath)), "Packs");
+        if (Directory.Exists(packsDir))
+        {
+            foreach (var msFile in Directory.GetFiles(packsDir, "*.ms"))
+            {
+                wz.LoadMsFile(msFile);
+            }
+        }
+    }
+}
+else
+{
+    wz.Load(wzFilePath, true);
+}
+
+Console.WriteLine("Base.wz open complete.");
+
+#endregion
+
+Console.WriteLine("Done");
